2f0fffd13733071d7e788b72e22d91264e6e444c
VadymMakovii <makovii.vadym@gmail.com>
2022-12-03 18:22:08 +0200

    Scroll changed to 580 px

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -77,7 +77,7 @@ export class ImageGallery extends Component {
     }
 
     if (prevState.images && prevState.images !== nextImages) {
-      window.scrollBy({ top: 600, behavior: 'smooth' });
+      window.scrollBy({ top: 580, behavior: 'smooth' });
     }
   }
 
