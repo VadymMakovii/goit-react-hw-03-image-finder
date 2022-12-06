d9818adcbe97c3e5ddb2976ad5da13f22a922593
VadymMakovii <makovii.vadym@gmail.com>
2022-12-06 21:50:42 +0200

    Changed components ImageGallery and App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Searchbar } from 'components/Searchbar/Searchbar';
@@ -7,27 +7,41 @@ import { Container } from './App.styled';
 
 export class App extends Component {
   state = {
+    page: 1,
+    images: [],
     request: '',
-    showGallery: false,
   };
 
-  componentDidUpdate(__, prevState) {
-    if (this.state.request !== prevState.request) {
-      this.setState({ showGallery: true });
-    }
-  }
-
   formSubmitHandler = ({ request }) => {
-    this.setState({ request, showGallery: false });
+    if (this.state.request === request) {
+      return toast.info(
+        'Please enter a new request or continue browsing current images'
+      );
+    }
+    this.setState({ request, page: 1, images: [] });
+  };
+
+  loadMoreClick = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  getNewImages = newImages => {
+    this.setState(({ images }) => ({ images: [...images, ...newImages] }));
   };
 
   render() {
-    const { request, showGallery } = this.state;
+    const { request, page, images } = this.state;
 
     return (
       <Container>
         <Searchbar onSubmit={this.formSubmitHandler} />
-        {showGallery && <ImageGallery request={request} />}
+        <ImageGallery
+          request={request}
+          page={page}
+          images={images}
+          onClick={this.loadMoreClick}
+          onFetchImages={this.getNewImages}
+        />
         <ToastContainer
           position="top-left"
           autoClose={1000}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { getImages, addImages } from '../../services/pixabay-api';
+import { getImages } from '../../services/pixabay-api';
 import { toast } from 'react-toastify';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Button } from 'components/Button/Button';
@@ -10,59 +10,39 @@ import { ImageList } from './ImageGallery.styled';
 export class ImageGallery extends Component {
   static propTypes = {
     request: PropTypes.string,
+    page: PropTypes.number.isRequired,
+    images: PropTypes.array.isRequired,
+    onClick: PropTypes.func.isRequired,
+    onFetchImages: PropTypes.func.isRequired,
   };
 
   state = {
-    page: 1,
-    images: [],
-    request: '',
     loading: false,
     loadingMore: false,
   };
 
-  async componentDidMount() {
-    const userRequest = this.props.request;
+  async componentDidUpdate(prevProps) {
+    const { request, page, onFetchImages } = this.props;
+    console.log(prevProps.request !== request);
 
-    this.setState({
-      request: userRequest,
-      loading: true,
-    });
-
-    try {
-      const response = await getImages(userRequest);
-      const results = response.hits.map(
-        ({ id, webformatURL, largeImageURL, tags }) => ({
-          id,
-          webformatURL,
-          largeImageURL,
-          tags,
-        })
-      );
-
-      this.setState({ images: results });
-      results.length < 1 &&
-        toast.warn(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      if (results.length < response.total) {
-        this.setState({ loadingMore: true });
-      }
-    } catch (error) {
-      toast.error(`${error.message}`);
-    } finally {
-      this.setState({ loading: false });
-    }
-  }
-
-  async componentDidUpdate(__, prevState) {
-    const { request, page } = this.state;
-
-    if (prevState.page !== page) {
-      this.setState({ loading: true, loadingMore: false });
+    if (prevProps.request !== request || prevProps.page !== page) {
+      this.setState({
+        loading: true,
+        loadingMore: false,
+      });
 
       try {
-        const response = await addImages(request, page);
-        const newImages = response.hits.map(
+        const response = await getImages(request, page);
+
+        response.hits.length < 1 &&
+          toast.warn(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+
+        response.hits.length < response.total &&
+          this.setState({ loadingMore: true });
+
+        const results = response.hits.map(
           ({ id, webformatURL, largeImageURL, tags }) => ({
             id,
             webformatURL,
@@ -71,15 +51,13 @@ export class ImageGallery extends Component {
           })
         );
 
-        this.setState(({ images }) => ({ images: [...images, ...newImages] }));
-        if (newImages.length < response.total) {
-          this.setState({ loadingMore: true });
-        }
+        onFetchImages(results);
       } catch (error) {
         toast.error(`${error.message}`);
       } finally {
         this.setState({ loading: false });
       }
+      return;
     }
 
     if (page > 1) {
@@ -87,12 +65,9 @@ export class ImageGallery extends Component {
     }
   }
 
-  loadMoreClick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
   render() {
-    const { images, loading, loadingMore } = this.state;
+    const { loading, loadingMore } = this.state;
+    const { onClick, images } = this.props;
 
     return (
       <>
@@ -109,7 +84,7 @@ export class ImageGallery extends Component {
           </ImageList>
         )}
         {loading && <Loader />}
-        {loadingMore && <Button onClick={this.loadMoreClick} />}
+        {loadingMore && <Button onClick={onClick} />}
       </>
     );
   }
